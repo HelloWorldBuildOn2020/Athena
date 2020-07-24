1657c7ae54bfe82a900dbbb24945e6c2d11cf1ff
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2020-07-25 01:40:02 +0700

    :lipstick: Add-file-config-color-and-font-size-style 🐯

---
+++ front-end/src/Config/Color.js
@@ -0,0 +1,16 @@
+export default {
+    secondary: '#40D38D',
+    primary: '#00B34B',
+    white: '#FFFFFF',
+    description: '#BDBDBD',
+    bgPrimary: '#FAFAFA',
+    bgSecondary: '#',
+    red: '#FF4012',
+    pink: '#FF80AB',
+    purple: '#AB47BC',
+    blue: '#3FB4FF',
+    green: '#00B34B',
+    yellow: '#FDD835',
+    orange: '#FF9800',
+    brown: '#795548'
+  }
