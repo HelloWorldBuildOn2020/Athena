37fdb429bc460ffb7f710e4255d2f1e609679348
kaotu <kao-tu@hotmail.co.th>
2020-07-28 00:13:49 +0700

    🔥 Remove command pull images 🌚

--- scripts/run-compose.sh
+++ scripts/run-compose.sh
@@ -3,7 +3,4 @@
 echo Loggin AWS ECR
     $(aws ecr get-login --region ap-southeast-2 --no-include-email)
 
-docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:FE_18
-docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:athena-nginx-front-end
-
 docker-compose -f /home/centos/docker-compose.deploy.yml up -d
