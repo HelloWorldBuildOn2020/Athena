94ca87f51df776af89b71de76716a75bdfe8979f
kaotu <kao-tu@hotmail.co.th>
2020-07-27 22:00:23 +0700

    Add sudo 🌚

--- scripts/run-compose.sh
+++ scripts/run-compose.sh
@@ -3,7 +3,7 @@
 echo Loggin AWS ECR
     $(aws ecr get-login --region ap-southeast-1 --no-include-email)
 
-docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:FE_18
-docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:athena-nginx-front-end
+sudo docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:FE_18
+sudo docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:athena-nginx-front-end
 
-docker-compose -f /home/centos/docker-compose.deploy.yml up -d
+sudo docker-compose -f /home/centos/docker-compose.deploy.yml up -d
