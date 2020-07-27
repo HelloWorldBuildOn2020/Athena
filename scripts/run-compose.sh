714f65cb87f381a62bedbe3472776b446c977660
kaotu <kao-tu@hotmail.co.th>
2020-07-27 21:28:36 +0700

    👷 Add appspec for ci 🌚

---
+++ scripts/run-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Loggin AWS ECR
+    $(aws ecr get-login --region ap-southeast-1 --no-include-email)
+
+docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:FE_18
+docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:athena-nginx-front-end
+
+docker-compose -f /home/centos/docker-compose.deploy.yml up -d
