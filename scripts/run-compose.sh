dea639c5a9db26cbbe43ff523e33e0762e33620e
kaotu <kao-tu@hotmail.co.th>
2020-07-27 22:34:58 +0700

    Update region 🌚

--- scripts/run-compose.sh
+++ scripts/run-compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Loggin AWS ECR
-    $(aws ecr get-login --region ap-southeast-1 --no-include-email)
+    $(aws ecr get-login --region ap-southeast-2 --no-include-email)
 
 docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:FE_18
 docker pull 192479299400.dkr.ecr.ap-southeast-2.amazonaws.com/athena:athena-nginx-front-end
