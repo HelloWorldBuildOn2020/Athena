5ed19766d4f221a70e2a60d43cbc7f5e0f451bae
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2020-07-28 20:40:29 +0700

    💄 Update style loader  🐯

--- front-end/src/components/Core/Loader.js
+++ front-end/src/components/Core/Loader.js
@@ -1,17 +1,11 @@
-import styled from 'styled-components'
-import color from '../../Config/Color'
+import React from 'react'
 
-export const Loader = styled.button`
-    border: 10px solid ${color.bgSecondary};
-    border-top: 10px solid ${color.primary};
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-    animation: spin 2s linear infinite;
-  
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }
-`
+const Loader = () => {
+  return (
+    <div class="spinner-border text-success" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  )
+}
 
+export default Loader
