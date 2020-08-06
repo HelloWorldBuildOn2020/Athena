9dcddc32e7cf08269675002b4f59b8461d206a76
kaotu <32597649+kaotu@users.noreply.github.com>
2020-08-07 01:34:04 +0700

    Update script 🌚

--- scripts/run-compose.sh
+++ scripts/run-compose.sh
@@ -3,5 +3,5 @@
 echo Loggin AWS ECR
     $(aws ecr get-login --region ap-southeast-2 --no-include-email)
 
-docker-compose -f docker-compose.deploy.yml pull
+docker-compose -f /home/centos/docker-compose.deploy.yml pull
 docker-compose -f /home/centos/docker-compose.deploy.yml up -d
