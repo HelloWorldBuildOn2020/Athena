5d9ac51ca4637a9bf1c37b11859eaa03443e9001
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2020-09-10 05:45:23 +0700

    Create file index.js for Athena 🐯

---
+++ front-end/src/components/Athena/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sider from '../Core/Sider'
+import { CustomContainer } from '../Core/Background'
+
+function Athena (props) {
+    
+  return(
+    <div>
+      <Sider/>
+    </div>
+  )
+}
+
+export default (Athena)
