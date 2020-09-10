51abc1188dcabf695a6306115604d686bb8ed17c
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2020-09-10 18:33:22 +0700

    Create mock data for Transaction 🐯

---
+++ front-end/src/utils/mock.js
@@ -0,0 +1,30 @@
+export const dataTransactions = [
+  {
+    key: "1",
+    charges: "$4500",
+    verification_code: "tran_id0001",
+    verify_status: "Successful",
+    date: "July 22, 2020 22:22",
+  },
+  {
+    key: "1",
+    charges: "$2350",
+    verification_code: "tran_id0002",
+    verify_status: "Failure",
+    date: "July 22, 2020 22:00",
+  },
+  {
+    key: "1",
+    charges: "$1500",
+    verification_code: "tran_id0003",
+    verify_status: "Successful",
+    date: "July 22, 2020 21:22",
+  },
+  {
+    key: "1",
+    charges: "$6700",
+    verification_code: "tran_id0004",
+    verify_status: "Failure",
+    date: "July 22, 2020 12:00",
+  },
+];
