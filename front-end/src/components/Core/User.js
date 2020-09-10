c3a414bb952537614fdbf1c7849f6047b5457f1d
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2020-09-10 23:58:44 +0700

    Create component User 🐯

---
+++ front-end/src/components/Core/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Col, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { SubTitle } from "./Text";
+
+const User = (props) => {
+  return (
+    <Col className="col-10 d-flex justify-content-end" style={{ padding: 10 }}>
+      <Avatar size="large" icon={<UserOutlined />} />
+      <SubTitle style={{ margin: "-10px 16px" }}>Test</SubTitle>
+    </Col>
+  );
+};
+
+export default User;
